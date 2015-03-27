bf3b65d56d04715d2fd6d52137a9c238fb68a3c6
U-ILZ-ORANGE\ilya <ilya_dreytser@yahoo.com>
2015-03-27 10:40:47 -0400

    Added CLI

---
+++ CLItool/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:9000/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // HTTP GET
+                HttpResponseMessage response = await client.GetAsync("api/products/1");
+                if (response.IsSuccessStatusCode)
+                {
+                    Product product = await response.Content.ReadAsAsync<Product>();
+                    Console.WriteLine("{0}\t${1}\t{2}", product.Name, product.Price, product.Category);
+                }
+
+                // HTTP POST
+                var gizmo = new Product() { Name = "Gizmo", Price = 100, Category = "Widget" };
+                response = await client.PostAsJsonAsync("api/products", gizmo);
+                if (response.IsSuccessStatusCode)
+                {
+                    Uri gizmoUrl = response.Headers.Location;
+
+                    // HTTP PUT
+                    gizmo.Price = 80;   // Update price
+                    response = await client.PutAsJsonAsync(gizmoUrl, gizmo);
+
+                    // HTTP DELETE
+                    response = await client.DeleteAsync(gizmoUrl);
+                }
+            }
+        }
+    }
+}
---
+++ WebApp/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Hello = "Aaron";
+            return View();
+        }
+
+        public JsonResult lookAtMe()
+        {
+            return Json(new { Aaron = "Tired", Ilya = "More Tired" }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
