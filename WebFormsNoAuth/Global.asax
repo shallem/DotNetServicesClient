d28a1c28a9d433ef2c57dd3f5e01ee9ffa187639
General Gau <timelyfeedback@gmail.com>
2016-08-11 10:44:48 -0400

    removed spurious test code not related to the W&C app

--- WebFormsNoAuth/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebFormsNoAuth.Global" Language="C#" %>
--- WebFormsNoAuth/Scripts/_references.js
+++
@@ -1,5 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="bootstrap.js" />
-/// <reference path="jquery-2.0.3.js" />
-/// <reference path="modernizr-2.7.1.js" />
-/// <reference path="respond.js" />
